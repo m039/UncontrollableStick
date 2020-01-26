073657e48b3286a6d559db306d31b1f82842527b
Dmitry Mozgin <m0391n@gmail.com>
2020-01-26 20:11:15 +0300

    Fixed stars in WinScreen.

--- Screens/WinScreen.gd
+++ Screens/WinScreen.gd
@@ -3,7 +3,7 @@ extends Node2D
 func _ready():
 	# Hide all stars
 	for i in range($Stars.get_child_count()):
-		$Stars.get_child(i).hide()
+		$Stars.get_child(i).get_node("Normal").hide()
 
 func set_number_of_stars(number : int):
 	# Show stars according to the number.
@@ -12,4 +12,4 @@ func set_number_of_stars(number : int):
 			break
 		else:
 			number -= 1
-			$Stars.get_child(i).show()
+			$Stars.get_child(i).get_node("Normal").show()
