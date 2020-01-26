74d7456b23ff49840e2ea45f5065e1f78bd0306f
Dmitry Mozgin <m0391n@gmail.com>
2020-01-26 21:13:29 +0300

    Added a link to the twitter

--- Screens/AboutScreen.gd
+++ Screens/AboutScreen.gd
@@ -2,3 +2,6 @@ extends Node2D
 
 func _on_BackButton_pressed():
 	get_tree().change_scene("res://Screens/MenuScreen.tscn")
+
+func _on_TwitterButton_pressed():
+	OS.shell_open("https://twitter.com/m0391n")
