d2c81d54282888533dee20ea650253cca2600bf6
Dmitry Mozgin <m0391n@gmail.com>
2020-01-26 21:00:00 +0300

    Added about screen

--- Screens/MenuScreen.gd
+++ Screens/MenuScreen.gd
@@ -2,3 +2,6 @@ extends Node2D
 
 func _on_StartGameButton_pressed():
 	get_tree().change_scene("res://Levels/Level1.tscn")
+
+func _on_AboutButton_pressed():
+	get_tree().change_scene("res://Screens/AboutScreen.tscn")
